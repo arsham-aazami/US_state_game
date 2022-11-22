629ce378f72b6ff006f562870dfd2934ed7f7321
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-11-22 23:34:51 +0330

    adding filtering data feature

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ screen.bgpic("blank_states_img.gif")
 Score()
 
 # asking the user to guess
-# Input(input("Enter state: "))
-
+user_choice = screen.textinput(title="Enter the state ", prompt="What another state's name?")
 # datas
 datas = pandas.read_csv("50_states.csv")
+if user_choice in datas["state"].tolist():
+	state_name = datas[datas["state"] == user_choice]["state"]
+	print(state_name)
+else:
+	print("there is no state with this name")
 
 screen.mainloop()
 screen.exitonclick()
