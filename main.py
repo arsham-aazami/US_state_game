acb0842a19af7088f30b0496da8c0ab6bc08a601
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-11-26 13:30:08 +0330

    detecting states

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 from turtle import Turtle, Screen
-
 import pandas
 from score import Score
 
 pointer = Turtle()
+pointer.penup()
 screen = Screen()
 
 screen.bgpic("blank_states_img.gif")
-Score()
+score = Score()
 
 # asking the user to guess
-user_choice = screen.textinput(title="Enter the state ", prompt="What another state's name?")
+
 # datas
 datas = pandas.read_csv("50_states.csv")
-if user_choice in datas["state"].tolist():
-	state_name = datas[datas["state"] == user_choice]["state"]
-	print(state_name)
-else:
-	print("there is no state with this name")
+all_states = datas["state"].tolist()
+
+correct_guess = 0
+while correct_guess < 50:
+    user_choice = screen.textinput(title="Enter the state ", prompt="What another state's name?").title()
+    if user_choice in all_states:
+        state_name = datas[datas["state"] == user_choice]["state"]
+        xcor = datas[datas["state"] == user_choice]["x"]
+        ycor = datas[datas["state"] == user_choice]["y"]
+        text = Turtle()
+        text.color("black")
+        text.hideturtle()
+        text.penup()
+        text.speed(0)
+        text.goto(int(xcor), int(ycor))
+        pointer.goto(int(xcor), int(ycor))
+        text.write(state_name.item(), font=("Arial", 10, "normal"))
+        print(state_name)
+    else:
+        print("there is no state with this name")
+
+    correct_guess += 1
 
 screen.mainloop()
-screen.exitonclick()
